628af5d2143108a16d93f0d545e32262ba8c0b31
Amanda <Amanda@>
2018-12-27 17:21:22 +0100

    Add project files.

---
+++ DejtingsajtProjekt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DejtingsajtProjekt.Startup))]
+namespace DejtingsajtProjekt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
