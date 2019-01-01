a90a27ccad8564c10ecf0f3ce09168413e68fafe
AmandaHaugland <amanda.haugland97@gmail.com>
2019-01-01 23:01:39 +0100

    Påbörjat förslag model för profile. Endast model än så länge

---
+++ DejtingsajtProjekt/Models/Profile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DejtingsajtProjekt.Models
+{
+    public class Profile
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime Birthday { get; set; }
+
+        //Foreign key till User
+    }
+}
