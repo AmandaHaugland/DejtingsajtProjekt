9c791050893a9c9280ce3cfea9af7d64cf0c29ed
AmandaHaugland <amanda.haugland97@gmail.com>
2019-01-08 12:53:48 +0100

    "Flyttat" på db

---
+++ DejtingsajtProjekt/Migrations/201901081145488_DBnowVisible.cs
@@ -0,0 +1,16 @@
+namespace DejtingsajtProjekt.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DBnowVisible : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
